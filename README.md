73ccbd60ac7d3661a0f129b780c0f2f2e879896f
James Jackson <jcj@alta.slack.net>
2014-11-22 12:53:39 -0500

    added update local to repo

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-tool for iterating through local git repo branches and pulling each from origin
+.. probably better ways to do this but i didn't see any with a few quick google searches... 
+
+gitall - tool for iterating through local git repo branches and pulling each from origin
+update_locla_repo.sh - tool for cron updates to local copy of repo
+
---
+++ update_local_repos.sh
@@ -0,0 +1,30 @@
+## I'M USING GITHUB BUT YOU CAN USE OTHER OF COURSE
+GITUSER=git
+GITREPO=github
+
+## CHNAGE THIS DIR TO BE WHERE YOU STORE YOUR GIT REPOS LOCAL 
+LOCAL_DIR=/home/jcj/git
+
+## USAGE - NOTE, DON'T INCLUDE THE .git 
+## ./upate_local_repos.sh PATH/REPP REPO/PATH 
+
+
+
+for i in $@; do
+    echo 
+    cd $LOCAL_DIR
+    echo "Working on $i"
+    if [ -d $i ]; then
+	cd ${i};
+
+	## MAKE SURE GITALL IS IN YOUR PATH
+	gitall
+
+    else
+	cd $LOCAL_DIR
+	mkdir -p ${i}
+	echo "RUNNING: git clone ${GITUSER}@{$GITREPO}:${i}.git ${i}"
+	git clone ${GITUSER}@{$GITREPO}:${i}.git ${i}
+    fi
+done
+
